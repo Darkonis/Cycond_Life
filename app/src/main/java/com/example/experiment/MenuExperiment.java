8daa0736089f266e037fe2560506c5fc0443216b
Gabriel_Rueger <gmrueger@iastate.edu>
2019-02-04 00:59:51 -0600

    initial commit

---
+++ app/src/main/java/com/example/experiment/MenuExperiment.java
@@ -0,0 +1,13 @@
+package com.example.experiment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuExperiment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_experiment);
+    }
+}
