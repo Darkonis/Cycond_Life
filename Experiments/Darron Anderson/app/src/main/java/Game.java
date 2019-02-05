bdf47960204e7ab3fa8fd5c7f82e41e5be5d5f24
Gabriel_Rueger <gmrueger@iastate.edu>
2019-02-05 11:25:03 -0600

    Revert "Merge branch 'master' of https://git.linux.iastate.edu/CS309SPRING2019/team_53"
    This reverts commit 75688a5bda26288ad43cebf25e8151ca9b95489d, reversing
    changes made to 7709a2feb3d1cfc872420d7a8bdf892d19c2775b.

---
+++ Experiments/Darron Anderson/app/src/main/java/Game.java
@@ -0,0 +1,48 @@
+import com.example.cycondlife.Character;
+
+import java.util.Random;
+import java.util.ArrayList;
+
+public class Game {
+    //TODO make all the rest control from here
+    ArrayList<Character> monster_map=new ArrayList<Character>();
+    //how large the generated area should be
+    double world_bounds = .2;
+    int num_monsters=0;
+    //TODO make this based of time
+    Random rand=new Random(0);
+    Game()
+    {
+        generate_mMap();
+    }
+    void generate_mMap()
+    {
+
+        while(num_monsters<50);
+        {
+            generate_monster();
+        }
+    }
+    void generate_monster()
+    {
+        boolean valid=false;
+        while(!valid) {
+            //add ames base
+            double lat = (rand.nextInt() % 100) / 1000.0 + 42.03 - .05;
+            double lon = (rand.nextInt() % 100) / 1000.0 + 93.63 - .05;
+            for (int i=0;i<num_monsters;i++)
+            {
+                if (Math.abs(lat-monster_map.get(i).get_latitude())<.001&&Math.abs(lon-monster_map.get(i).get_longitude())<.001)
+                {
+                    break;
+                }
+                if(num_monsters-i==1)
+                {
+                    monster_map.add(new Character(lat,lon));
+                    num_monsters++;
+                    valid=true;
+                }
+            }
+        }
+    }
+}
