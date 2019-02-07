e65ca048d87287f62271a9a05320146783f38167
cndallas <cndallas@cndallas-Z9PE-D8-WS>
2019-02-07 15:15:09 -0600

    Simple user controller outline. Nothing functional has been done yet.

---
+++ Backend/src/main/java/edu/se309/app/backend/Se309RpgAppApplication.java
@@ -0,0 +1,14 @@
+package edu.se309.app.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Se309RpgAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Se309RpgAppApplication.class, args);
+	}
+
+}
+
