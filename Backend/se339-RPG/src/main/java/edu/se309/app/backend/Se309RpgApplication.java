63d677236f9049232a4cfd6e5da4f064e9dde722
CNDallas <cndallas@cndallas-Z9PE-D8-WS>
2019-02-07 21:09:29 -0600

    Name changes because I can make mistakes from time to almost all of the time. Plus adding in another dummy endpoint

--- Backend/se339-RPG/src/main/java/edu/se309/app/backend/Se309RpgApplication.java
+++ Backend/se339-RPG/src/main/java/edu/se309/app/backend/Se309RpgApplication.java
@@ -1,13 +1,13 @@
-package edu.se339.app.backend;
+package edu.se309.app.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Se339RpgApplication {
+public class Se309RpgApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Se339RpgApplication.class, args);
+		SpringApplication.run(Se309RpgApplication.class, args);
 	}
 
 }
--- Backend/se339-RPG/src/test/java/edu/se309/app/backend/Se309RpgApplicationTests.java
+++ Backend/se339-RPG/src/test/java/edu/se309/app/backend/Se309RpgApplicationTests.java
@@ -1,4 +1,4 @@
-package edu.se339.app.backend;
+package edu.se309.app.backend;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class Se339RpgApplicationTests {
+public class Se309RpgApplicationTests {
 
 	@Test
 	public void contextLoads() {
