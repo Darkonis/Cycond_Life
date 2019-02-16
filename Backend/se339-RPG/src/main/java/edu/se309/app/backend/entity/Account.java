b5b8cc6186b5c18393ff4096c99b4f79ecf93994
CNDallas <cndallas@cndallas-Z9PE-D8-WS>
2019-02-15 23:57:28 -0600

    API for account creation. No security or service layer added yet. Also haven't been able to test because my local DB is being screwy. Will test later and update if necessary.

---
+++ Backend/se339-RPG/src/main/java/edu/se309/app/backend/entity/Account.java
@@ -0,0 +1,121 @@
+package edu.se309.app.backend.entity;
+
+
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name="accounts")
+public class Account {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="account_id")
+	private int accountId;
+	
+	@Column(name="username", nullable = false)
+	private String username;
+	
+	@Column(name="password", nullable = false)
+	private String password;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	@Column(name="email", nullable = false)
+	private String email;
+	
+	@Column(name="created_on")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createdOn;
+	
+	@Column(name="account_type", nullable = false)
+	private String accountType;
+	
+	public Account() {
+		
+	}
+
+	public Account(String username, String password, String firstName, String lastName, String email,
+			String accountType) {		
+		this.username = username;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.accountType = accountType;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAccountType() {
+		return accountType;
+	}
+
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+
+	@Override
+	public String toString() {
+		return "Accounts [accountId=" + accountId + ", username=" + username + ", firstName="
+				+ firstName + ", lastName=" + lastName + ", email=" + email + ", createdOn=" + createdOn
+				+ ", accountType=" + accountType + "]";
+	}
+	
+	
+	
+	
+	
+}
