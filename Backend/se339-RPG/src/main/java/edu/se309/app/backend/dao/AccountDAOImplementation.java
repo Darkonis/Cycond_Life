36c67c4dc282034f99eda565b957ae6c37954734
Darkonis <flameshadow98@yahoo.com>
2019-02-18 14:23:33 -0600

    Merge branch 'Darron_Anderson' of C:\Users\flame\Desktop\Code\Cycond life\team_53 with conflicts.

---
+++ Backend/se339-RPG/src/main/java/edu/se309/app/backend/dao/AccountDAOImplementation.java
@@ -0,0 +1,87 @@
+package edu.se309.app.backend.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import edu.se309.app.backend.entity.Account;
+
+@Repository
+public class AccountDAOImplementation implements AccountDAO {
+
+	private EntityManager entityManager;
+	
+	@Autowired
+	public AccountDAOImplementation(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+	
+	@Override
+	public void deleteById(int accountId) {
+		Session currentSession = entityManager.unwrap(Session.class);
+		Query query = 
+				currentSession.createQuery("DELETE FROM Account where id=:accountId")
+				.setParameter("accountId", accountId);
+		query.executeUpdate();
+		
+	}
+
+	@Override	
+	public List<Account> findAll() {
+		
+		Session currentSession = entityManager.unwrap(Session.class);
+		
+		Query<Account> query = currentSession.createQuery("FROM Account", Account.class);
+		
+		List<Account> accounts = query.getResultList();
+		
+		return accounts;
+	}
+
+	@Override
+	public Account findByEmail(String email) {
+		Session currentSession = entityManager.unwrap(Session.class);
+		
+		Query<Account> query = 
+				currentSession.createQuery("FROM Account a WHERE a.email = :email", Account.class)
+				.setParameter("email", email);
+		
+		Account accountOutput = query.getSingleResult();
+		return accountOutput;
+	}
+
+	@Override
+	public Account findById(int accountId) {
+		
+		Session currentSession = entityManager.unwrap(Session.class);
+		
+		Account account = currentSession.get(Account.class, accountId);
+		
+		return account;
+	}
+
+	@Override
+	public Account findByUsername(String username) {
+		Session currentSession = entityManager.unwrap(Session.class);
+		
+		Query<Account> query = 
+				currentSession.createQuery("FROM Account a WHERE a.username = :username", Account.class)
+				.setParameter("username", username);
+		
+		Account accountOutput = query.getSingleResult();
+		return accountOutput;
+	}
+
+	@Override
+	public void save(Account newAccount) {
+		Session currentSession = entityManager.unwrap(Session.class);
+		currentSession.saveOrUpdate(newAccount);
+		
+	}
+
+}
---
+++ Backend/se339-RPG/src/main/java/edu/se309/app/backend/service/AccountService.java
@@ -0,0 +1,21 @@
+package edu.se309.app.backend.service;
+
+import java.util.List;
+
+import edu.se309.app.backend.entity.Account;
+
+public interface AccountService {
+	
+public void deleteById(int accountId);
+	
+	public List<Account> findAll();
+	
+	public Account findByEmail(String email);
+	
+	public Account findById(int accountId);
+	
+	public Account findByUsername(String username);
+	
+	public void save(Account newAccount);
+
+}
