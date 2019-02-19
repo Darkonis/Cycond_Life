fd7175471d3e4b52323ae2835710c2daf0f0d720
Darkonis <flameshadow98@yahoo.com>
2019-02-19 11:19:33 -0600

    Added combat sub menu to access long press on a marker on the map screen

--- Frontend/app/src/main/java/com/example/cycondlife/Combat.java
+++ Frontend/app/src/main/java/com/example/cycondlife/Combat.java
@@ -2,6 +2,7 @@ package com.example.cycondlife;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.Button;
 import android.view.View;
@@ -11,7 +12,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
-public class Combat extends Activity {
+public class Combat extends AppCompatActivity {
 
     Button button;
 
@@ -19,26 +20,8 @@ public class Combat extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.combat);
-        addListenerOnButton();
+        button = findViewById(R.id.run);
     }
-    public void addListenerOnButton() {
 
-        final Context context = this;
-
-        button = (Button) findViewById(R.id.button1);
-
-        button.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View arg0) {
-
-                Intent intent = new Intent(context, MapsActivity.class);
-                startActivity(intent);
-
-            }
-
-        });
-
-    }
 
 }
