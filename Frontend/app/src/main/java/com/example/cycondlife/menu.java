6a73628f8c08432a45dbe1821ec30a9a621a84ce
Darkonis <flameshadow98@yahoo.com>
2019-03-05 12:09:26 -0600

    added dice class and untester monster list call

--- Frontend/app/src/main/java/com/example/cycondlife/menu.java
+++ Frontend/app/src/main/java/com/example/cycondlife/menu.java
@@ -1,5 +1,6 @@
 package com.example.cycondlife;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,13 +9,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+
 public class menu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-
+        pull_monster_map();
         //button object creations
         Button stats = findViewById(R.id.stats);
         Button inventory = findViewById(R.id.inventory);
@@ -44,7 +53,53 @@ public class menu extends AppCompatActivity {
 
 
     }
+    private void pull_monster_map()
+    {
+        Context c= getApplicationContext();
+        final RequestQueue requestQueue = Volley.newRequestQueue(c);
+        // Initialize a new JsonArrayRequest instance
 
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                "http://cs309-sd-6.misc.iastate.edu:8080/api/monster/list",
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        // Do something with response
+                        //mTextView.setText(response.toString());
+
+                        // Process the JSON
+                        Log.i("Cycond test", "request succsessful");
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+                                String[] out = new String[response.length()];
+                                for (int k = 0; k < response.length(); k++) {
+                                    out[k] = response.get(k).toString();
+
+                                    Log.i("Cycond life", "onResponse: " + out[k]);
+                                }
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Do something when error occurred
+                        Log.i("Cycond Life", "request error");
+                        Log.i("Cycond Life", error.getLocalizedMessage());
+                    }
+                }
+        );
+
+
+        // Add JsonArrayRequest to the RequestQueue
+        requestQueue.add(jsonArrayRequest);
+
+
+    }
     /* public void setActivityBackgroundColor(int color) {      //Can possibly be used to change background color of main menu
         View view = this.getWindow().getDecorView();
         view.setBackgroundColor(color);
