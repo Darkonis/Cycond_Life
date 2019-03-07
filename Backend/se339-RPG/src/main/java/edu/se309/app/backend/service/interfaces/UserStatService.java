9abf66f42ce36d3b906b1895097fff6bf2777f3f
Dallas Evers <cndallas@iastate.edu>
2019-03-07 11:47:11 -0600

    start of test cases

--- Backend/se339-RPG/src/main/java/edu/se309/app/backend/service/interfaces/UserStatService.java
+++ Backend/se339-RPG/src/main/java/edu/se309/app/backend/service/interfaces/UserStatService.java
@@ -3,7 +3,6 @@ package edu.se309.app.backend.service.interfaces;
 import java.util.List;
 import java.util.Optional;
 
-import edu.se309.app.backend.entity.Account;
 import edu.se309.app.backend.entity.UserStat;
 
 public interface UserStatService {
