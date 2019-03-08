d89ddd9f072c85ef7ba99242abf4466d8dbcf001
Dallas Evers <cndallas@iastate.edu>
2019-03-08 13:14:52 -0600

    Redirected base url (host:8080/) to go to the api documentation

--- Backend/se339-RPG/src/main/java/edu/se309/app/backend/common/config/SpringFoxConfig.java
+++ Backend/se339-RPG/src/main/java/edu/se309/app/backend/common/config/SpringFoxConfig.java
@@ -3,6 +3,7 @@ package edu.se309.app.backend.common.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import springfox.documentation.builders.PathSelectors;
@@ -14,10 +15,20 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SpringFoxConfig extends WebMvcConfigurationSupport {
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+	    registry.addRedirectViewController("/v2/api-docs", "/v2/api-docs");
+	    registry.addRedirectViewController("/configuration/ui", "/configuration/ui");
+	    registry.addRedirectViewController("/configuration/security", "/configuration/security");
+	    registry.addRedirectViewController("/swagger-resources", "/swagger-resources");
+	    registry.addRedirectViewController("/", "/swagger-ui.html");
+	   
+	}
+	
 	@Override
 	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-
+		registry.addResourceHandler("/**").addResourceLocations("classpath:/META-INF/resources/");		
 		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
 
