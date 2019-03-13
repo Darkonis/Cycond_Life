91dd836280074e8a17b585325d149301abade217
Maxwell Wangler <mwangler@iastate.edu>
2019-03-13 14:14:20 -0500

    Removed the print statements in the App

--- Backend/se339-RPG/src/main/java/edu/se309/app/backend/Se309RpgApplication.java
+++ Backend/se339-RPG/src/main/java/edu/se309/app/backend/Se309RpgApplication.java
@@ -16,20 +16,18 @@ public class Se309RpgApplication  {
     SpringApplication.run(Se309RpgApplication.class, args);
 	OffsetDateTime currentTime = OffsetDateTime.now();
 	OffsetDateTime regenTime = currentTime.plusSeconds(10);
-	OffsetDateTime respawnTime = currentTime.plusMinutes(1);
+	OffsetDateTime respawnTime = currentTime.plusMinutes(15);
 	while(true)
 	{
 		currentTime =OffsetDateTime.now();
 		if(currentTime.compareTo(regenTime) >= 0)
 		{
-			System.out.println("Health Regained");
 			regenTime = currentTime.plusSeconds(10);
 
 		}
 		if(currentTime.compareTo(respawnTime) >= 0)
 		{
-			System.out.println("Monsters Respawned");
-			respawnTime = currentTime.plusMinutes(1);
+			respawnTime = currentTime.plusMinutes(15);
 			String url = "http://cs309-sd-6.misc.iastate.edu:8080/api/monsters/generate";
 			URL obj = new URL(url);
 			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
