ff0df7f98fe869b07151d9286c136280e16526f9
Gabriel_Rueger <gmrueger@iastate.edu>
2019-04-02 15:20:49 -0500

    Fixed some crashing

--- Frontend/app/src/main/java/com/example/cycondlife/Chat.java
+++ Frontend/app/src/main/java/com/example/cycondlife/Chat.java
@@ -1,19 +1,24 @@
 package com.example.cycondlife;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 
 public class Chat extends AppCompatActivity {
 
-    Button sendBut;
-    TextView userMessage;
-    URI toUse;
+    private static Context context;
+    private Button sendBut;
+    private TextView userMessage;
+    private TextView chatText;
+    private URI toUse;
+    //private Toast noConnection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +27,12 @@ public class Chat extends AppCompatActivity {
 
         sendBut = findViewById(R.id.sendButton);
         userMessage = findViewById(R.id.userText);
+        chatText = findViewById(R.id.chatBox);
+        context = getApplicationContext();
+        //noConnection = new Toast.makeText(getChatContext(), "Failed to connect to server", Toast.LENGTH_SHORT);
 
         try {
-            toUse = new URI("ws://echo.websocket.org");
+            toUse = new URI("wss://echo.websocket.org");
         }   catch (URISyntaxException e)    {
             e.printStackTrace();
         }
@@ -43,6 +51,12 @@ public class Chat extends AppCompatActivity {
         ChatSender sender = new ChatSender();
         sender.connectWebSocket(toUse);
         sender.sendMsg(userMessage.getText().toString());
+
+        chatText.setText(sender.getReceivedStuff());
+    }
+
+    public static Context getChatContext()  {
+        return context;
     }
 
 
--- Frontend/app/src/main/java/com/example/cycondlife/ChatSender.java
+++ Frontend/app/src/main/java/com/example/cycondlife/ChatSender.java
@@ -1,23 +1,30 @@
 package com.example.cycondlife;
 
-import android.os.AsyncTask;
-
 import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.exceptions.WebsocketNotConnectedException;
 import org.java_websocket.handshake.ServerHandshake;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.Socket;
+import java.io.InputStreamReader;
 import java.net.URI;
-import java.net.URISyntaxException;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 public class ChatSender {
     //Socket chat;
 
+    String receivedText;
     WebSocketClient chat;
     //DataOutputStream dos;
-    PrintWriter pw;
+    //PrintWriter pw;
+    //Toast noConnection = new Toast.makeText(getChatContext(), "Failed to connect to server", Toast.LENGTH_SHORT);
 
 
     public Void connectWebSocket(URI dest)  {
@@ -25,7 +32,7 @@ public class ChatSender {
 
         chat = new WebSocketClient(dest)   {
             public void onMessage(String var1)  {
-
+                receivedText += receivedText + "\n" + var1;
             }
 
             public void onOpen(ServerHandshake var1)  {
@@ -41,7 +48,35 @@ public class ChatSender {
             }
         };
 
-        chat.connect();
+        //chat.connect();
+
+        //Following TrustManager and try/catch is ONLY for WWS server, otherwise just use chat.connect() above (presumably)
+        TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+            public X509Certificate[] getAcceptedIssuers() {
+                X509Certificate[] myTrustedAnchors = new X509Certificate[0];
+                return myTrustedAnchors;
+            }
+
+            @Override
+            public void checkClientTrusted(X509Certificate[] certs,
+                                           String authType) {}
+
+            @Override
+            public void checkServerTrusted(X509Certificate[] certs,
+                                           String authType) {}
+        } };
+
+        try {
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, trustAllCerts, new SecureRandom());
+            SSLSocketFactory factory = sslContext.getSocketFactory();
+            chat.setSocket(factory.createSocket());
+            chat.connectBlocking();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
 
 //        try {
 //            pw = new PrintWriter(chat.getSocket().getOutputStream());
@@ -52,11 +87,39 @@ public class ChatSender {
 //            e.printStackTrace();
 //        }
 
-
         return null;
     }
 
     public void sendMsg(String msg)  {
-        chat.send(msg);
+        try {
+            chat.send(msg);
+        }   catch (WebsocketNotConnectedException e) {
+            e.printStackTrace();
+            //noConnection = new Toast.makeText(getChatContext(), "Failed to connect to server", Toast.LENGTH_SHORT).show();
+        }
+
     }
+
+
+    //need a way to make background listener
+    public String getReceivedStuff()  {
+        BufferedReader in;
+
+        try {
+            in = new BufferedReader(new InputStreamReader(chat.getSocket().getInputStream()));
+
+            while (in.ready()) {
+                receivedText += "echo: " + in.readLine();
+            }
+
+            in.close();
+        }   catch (IOException e)   {
+            e.printStackTrace();
+        }
+
+
+
+        return receivedText;
+    }
+
 }
