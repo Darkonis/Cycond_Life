3180285087e7846d6f7b3f44391979c3c4598015
Gabriel_Rueger <gmrueger@iastate.edu>
2019-04-02 02:26:49 -0500

    Safety commit before I do something too stupid

--- Frontend/app/src/main/java/com/example/cycondlife/Chat.java
+++ Frontend/app/src/main/java/com/example/cycondlife/Chat.java
@@ -2,8 +2,14 @@ package com.example.cycondlife;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Chat extends AppCompatActivity {
+
+    Button sendBut = findViewById(R.id.send);
+    TextView userMessage = findViewById(R.id.userText);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,7 +17,10 @@ public class Chat extends AppCompatActivity {
         setContentView(R.layout.activity_chat);
     }
 
-
+    public void send(View v)    {
+        ChatSender sender = new ChatSender();
+        sender.execute(userMessage.getText().toString());
+    }
 
 
 }
--- Frontend/app/src/main/java/com/example/cycondlife/ChatSender.java
+++ Frontend/app/src/main/java/com/example/cycondlife/ChatSender.java
@@ -6,22 +6,25 @@ import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
 
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-public class ChatSender extends AsyncTask<Void, Void, Void> {
+public class ChatSender extends AsyncTask<String, Void, Void> {
     //Socket chat;
-    
+
     WebSocketClient chat;
     DataOutputStream dos;
     PrintWriter pw;
 
 
     @Override
-    protected Void doInBackground(Void... voids)    {
+    protected Void doInBackground(String... voids)    {
         //chat = new Socket();
+
+        String message = voids[0];
 
         try {
             chat = new WebSocketClient(new URI("wss://echo.websocket.org"))   {
@@ -41,8 +44,19 @@ public class ChatSender extends AsyncTask<Void, Void, Void> {
 
                 }
             };
+
+
         }   catch (URISyntaxException e)    {
             e.printStackTrace();
+        }
+
+        try {
+            pw = new PrintWriter(chat.getSocket().getOutputStream());
+            pw.write(message);
+            pw.flush();
+            pw.close();
+        }   catch (IOException e) {
+
         }
 
 
