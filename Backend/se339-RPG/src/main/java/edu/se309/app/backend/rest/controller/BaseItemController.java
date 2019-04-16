5897130af2cc5d8ac69dcdcac718ea7ba4836479
Maxwell Wangler <mwangler@iastate.edu>
2019-04-16 15:44:39 -0500

    Created Controllers for Item and Consumable Item

---
+++ Backend/se339-RPG/src/main/java/edu/se309/app/backend/rest/controller/BaseItemController.java
@@ -0,0 +1,17 @@
+package edu.se309.app.backend.rest.controller;
+
+import edu.se309.app.backend.rest.entity.BaseItem;
+import edu.se309.app.backend.rest.service.interfaces.BaseItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/items")
+public class BaseItemController extends BaseController<BaseItem, Integer, BaseItemService> {
+
+    @Autowired
+    public BaseItemController(BaseItemService baseItemService) {
+        super(baseItemService);
+    }
+}
