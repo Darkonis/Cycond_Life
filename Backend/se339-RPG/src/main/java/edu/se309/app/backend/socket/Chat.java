55f78b60a8efa1586e24f108f56ae383aed432e9
Dallas Evers <cndallas@iastate.edu>
2019-04-24 19:39:01 -0500

    Java Doc for socket Chat added

--- Backend/se339-RPG/src/main/java/edu/se309/app/backend/socket/Chat.java
+++ Backend/se339-RPG/src/main/java/edu/se309/app/backend/socket/Chat.java
@@ -12,15 +12,23 @@ import static edu.se309.app.backend.socket.WebSocketSharedSingleton.getAccountSe
 public class Chat {
 
 
-    public Chat (){
-
-    }
-
+    /**
+     * Sends a global message with the format "Chat {Username of sender} : {message being sent}"
+     * @param userSendingMessage username of sender
+     * @param globalMessage message to be broadcasted
+     */
     public void CHAT(String userSendingMessage, String globalMessage) {
         String chatMessage = "Chat " + userSendingMessage + ": " + globalMessage;
         broadcast(chatMessage);
     }
 
+    /**
+     * Sends a message to a particular user with the format "DM {Username of sender} : {message being sent}"
+     * @param toUser username of the receiving account
+     * @param fromUser username of the account sending the message
+     * @param message message to be sent
+     * @throws IOException
+     */
     public void DIRECT_MESSAGE(String toUser, String fromUser, String message) throws IOException {
         Account receivingUser = getAccountController().getAccountByUsername(toUser);
         Session receivingSession = getAccountSessionMap().get(receivingUser);
