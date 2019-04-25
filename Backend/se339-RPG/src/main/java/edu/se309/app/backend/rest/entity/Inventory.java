11b13a59ef7c5fc8ccd2137c025eed7cca1b3fe0
Dallas Evers <cndallas@iastate.edu>
2019-04-24 22:34:20 -0500

    Java Doc for inventory

--- Backend/se339-RPG/src/main/java/edu/se309/app/backend/rest/entity/Inventory.java
+++ Backend/se339-RPG/src/main/java/edu/se309/app/backend/rest/entity/Inventory.java
@@ -2,59 +2,99 @@ package edu.se309.app.backend.rest.entity;
 
 import javax.persistence.*;
 
+/**
+ * Class to represent inventory
+ */
 @Entity
 @Table(name = "inventory")
 public class Inventory {
-	
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
-    
+
     @Column(name = "player_id")
     private int playerId;
-    
+
     @Column(name = "item_id")
     private int itemId;
-    
+
     @Column(name = "item_name")
     private String itemName;
-    
+
+    /**
+     * Get ID
+     *
+     * @return ID of the inventory
+     */
     public int getId() {
-    	return id;
+        return id;
     }
-    
+
+    /**
+     * Get player ID
+     * @return player ID of the inventory
+     */
     public int getPlayerId() {
-    	return playerId;
+        return playerId;
     }
-    
+
+    /**
+     * Get item ID
+     * @return item ID of the inventory
+     */
     public int getItemId() {
-    	return itemId;
+        return itemId;
     }
-    
+
+    /**
+     * Get item name
+     * @return item name of the inventory
+     */
     public String getItemName() {
-    	return itemName;
+        return itemName;
     }
-    
+
+    /**
+     * Set ID
+     * @param id ID of the inventory
+     */
     public void setId(int id) {
-    	this.id = id;
+        this.id = id;
     }
-    
+
+    /**
+     * Set Player ID
+     * @param playerId player ID of the inventory
+     */
     public void setPlayerId(int playerId) {
-    	this.playerId = playerId;
+        this.playerId = playerId;
     }
-    
+
+    /**
+     * Set item ID
+     * @param itemId item ID of the inventory
+     */
     public void setItemId(int itemId) {
-    	this.itemId = itemId;
+        this.itemId = itemId;
     }
-    
+
+    /**
+     * Set item name
+     * @param itemName item name of the inventory
+     */
     public void setItemName(String itemName) {
-    	this.itemName = itemName;
+        this.itemName = itemName;
     }
-    
+
+    /**
+     * String representation of inventory
+     * @return string representation of inventory
+     */
     @Override
     public String toString() {
-    	return "Inventory [id=" + id + ", playerId=" + playerId + ", itemId=" + itemId 
-    			+ ", itemName=" + itemName + "]";
+        return "Inventory [id=" + id + ", playerId=" + playerId + ", itemId=" + itemId
+                + ", itemName=" + itemName + "]";
     }
 }
