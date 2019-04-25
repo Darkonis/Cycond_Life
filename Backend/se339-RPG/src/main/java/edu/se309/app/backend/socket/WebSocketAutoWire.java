3d4d9294610b3c9bc190c73e71f4a98d8b5dcc8e
Dallas Evers <cndallas@iastate.edu>
2019-04-24 19:45:00 -0500

    Java Doc for WebSocketAutoWire

--- Backend/se339-RPG/src/main/java/edu/se309/app/backend/socket/WebSocketAutoWire.java
+++ Backend/se339-RPG/src/main/java/edu/se309/app/backend/socket/WebSocketAutoWire.java
@@ -12,16 +12,31 @@ public class WebSocketAutoWire implements ApplicationContextAware {
 
     private static ApplicationContext applicationContext;
 
+    /**
+     * Used by Spring Boot and shouldn't be called by any other source
+     * @param applicationContext
+     * @throws BeansException
+     */
     @Override
     @Autowired
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         this.applicationContext = applicationContext;
     }
 
+    /**
+     * Returns the ApplicationContext
+     * @return ApplicationContext
+     */
     public static ApplicationContext getApplicationContext() {
         return applicationContext;
     }
 
+    /**
+     * Is used to get the stored Spring bean when autowiring such in isn't feasible
+     * @param beanClass The class of the requested spring bean
+     * @param <T> The object type of the request spring bean
+     * @return the instance of the spring bean
+     */
     public static <T> T getBean(Class<T> beanClass) {
         return applicationContext.getBean(beanClass);
     }
