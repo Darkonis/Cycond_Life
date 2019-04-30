db4da8c47e86f0eadf4da8cf68436e472c346306
Darkonis <flameshadow98@yahoo.com>
2019-04-29 20:22:54 -0500

    added the shop activity

---
+++ Frontend/app/src/main/java/com/example/cycondlife/screens/shop.java
@@ -0,0 +1,15 @@
+package com.example.cycondlife.screens;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.cycondlife.R;
+
+public class shop extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop);
+    }
+}
